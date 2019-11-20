014b821ab6517d12b17ee886136f41599b98f57e
Jared Forsyth <jared@jaredforsyth.com>
2019-11-20 16:54:07 -0700

    prettier

--- index.js
+++ index.js
@@ -1,53 +1,67 @@
 #!/usr/bin/env node
 
-const core = require('@actions/core');
-const {context, GitHub} = require('@actions/github')
+const core = require("@actions/core");
+const { context, GitHub } = require("@actions/github");
 
 async function run() {
-    const trigger = core.getInput('trigger');
+    const trigger = core.getInput("trigger");
     if (!trigger) {
-        core.setFailed('No `trigger` input given, aborting.')
-        return
+        core.setFailed("No `trigger` input given, aborting.");
+        return;
     }
-
-    if (context.eventName === 'issue_comment' && !context.payload.issue.pull_request) {
-        // not a pull-request comment, aborting
-        core.setOutput('triggered', 'false');
-        return
-    }
-
-    const {owner, repo} = context.repo;
-
-    const body = context.eventName === 'issue_comment'
-        ? context.payload.comment.body
-        : context.payload.pull_request.body;
-
-    if (!body.includes(trigger)) {
-        core.setOutput('triggered', 'false');
+    const reaction = core.getInput("reaction");
+    const { GITHUB_TOKEN } = process.env;
+    if (reaction && !GITHUB_TOKEN) {
+        core.setFailed('If "reaction" is supplied, GITHUB_TOKEN is required');
         return;
     }
 
-    core.setOutput('triggered', 'true');
+    if (
+        context.eventName === "issue_comment" &&
+        !context.payload.issue.pull_request
+    ) {
+        // not a pull-request comment, aborting
+        core.setOutput("triggered", "false");
+        return;
+    }
 
-    const client = new GitHub(process.env.GITHUB_TOKEN);
-    if (context.eventName === 'issue_comment') {
+    const { owner, repo } = context.repo;
+
+    const body =
+        context.eventName === "issue_comment"
+            ? context.payload.comment.body
+            : context.payload.pull_request.body;
+
+    if (!body.includes(trigger)) {
+        core.setOutput("triggered", "false");
+        return;
+    }
+
+    core.setOutput("triggered", "true");
+
+    if (!reaction) {
+        return;
+    }
+
+    const client = new GitHub(GITHUB_TOKEN);
+    if (context.eventName === "issue_comment") {
         await client.reactions.createForIssueComment({
             owner,
             repo,
             comment_id: context.payload.comment.id,
-            content: 'rocket'
-        })
+            content: reaction
+        });
     } else {
         await client.reactions.createForIssue({
             owner,
             repo,
             issue_number: context.payload.pull_request.number,
-            content: 'rocket'
+            content: reaction
         });
     }
 }
 
 run().catch(err => {
-    console.error(err)
-    core.setFailed('Unexpected error');
+    console.error(err);
+    core.setFailed("Unexpected error");
 });
